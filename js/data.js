60cef018ae041ddf9f662bca89b3b94aa7867441
Napster777 <jorge.zapata@protonmail.com>
2021-08-18 21:58:16 -0600

    webform integrate api

---
+++ js/data.js
@@ -0,0 +1,30 @@
+function sendData(){
+  let name = document.getElementById("name").value;
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+
+  let data = {
+    "name": name,
+    "username": username,
+    "password": password
+  }
+
+  let header = new Headers();
+  debugger;
+  console.log(header);
+
+  let res = fetch("http://127.0.0.1:8000/v1/webform/", {
+    method: 'POST',
+    body: JSON.stringify(data),
+    mode: "cors",
+    headers: header,
+  }).then(function (response){
+    if (response.ok){
+      console.log("Successfully!!!");
+    } else {
+      console.log(response);
+    }
+  })
+
+  console.log(res);
+}
